f69d72cc3658af8a6af9ba33a77e9caae61682f5
Dmitry Marchenko <dmitry_marchenko@inbox.ru>
2017-02-16 17:55:28 +0300

    update readme.md, add comments in scripts, start frontend

--- README.md
+++ README.md
@@ -16,4 +16,17 @@ Unpack archive and run this commands
 $ cd neural-network/core/  
 $ make prepare  
 $ make  
-$ ./bin/nn
+For launch use  
+$ ./bin/nn  
+Parameters list:  
+-v (--version) - print version and developer  
+-h (--help) - print help  
+-l (--learn) - launch neural network for learning (required params: -s, -d, -i, -o)  
+-t (--test) - testing neural network (required params: -d, -i)  
+-p (--process) - processing neural network (required params: -d, -i)  
+-i (--input) - input file with scheme (and weights for neurons)  
+-o (--output) - output file for learning, testing or processing   result
+-d (--data) -  data file for learning, testing or processing neural network  
+--iteration - counts of learning iteration (if data file is large)
+--log - print info about learning each N iterations  
+If you launch without --output, then application will printing in stdout.   
--- prepare/convert.py
+++ prepare/convert.py
@@ -4,12 +4,15 @@ import math
 fileold = open('timeline.txt', 'r')
 filenew = open('classes.txt', 'w')
 
-roundcount = 4
-timeline = []
-classes = []
+roundcount = 4 # кол-во знаков после запятой (для округления)
+timeline = []  # временной ряд
+classes = []   # список классов
 
+# считываем временной ряд
 for n in fileold:
     timeline.append(round(float(n), roundcount))
+
+# создаём список классов вида (значение, кол-во повторений)
 for i, v1 in enumerate(timeline):
     found = False
     for v2 in classes:
@@ -20,10 +23,14 @@ for i, v1 in enumerate(timeline):
     if not found:
         item = [v1, 1]
         classes.append(item)
+
+# печатаем список классов и считаем уникальные значения
 one = 0
 for n in classes:
     if n[1]==1: one += 1
     print(n[0], n[1])
+
+# выводим статистику временного ряда и списка классов
 print('всего: '+str(len(timeline)))
 print('неповторяющихся : '+str(len(classes)))
 print('уникальных: '+str(one))
